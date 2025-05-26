f80f8d67cf563121b9ec197dccb5f516440a91b2
Prakhar <prakharb5645@gmail.com>
2025-05-26 09:22:30 +0530

    more question

---
+++ more_practice/1_LCM.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int GCD = gcd(num1, num2);
+
+    int LCM = (num1 * num2) / GCD;
+    printf("GCD of %d and %d is %d\n", num1, num2, LCM);
+    return 0;
+}
