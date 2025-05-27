618917db212792b4a6158c38f17cf3308d014cf8
Prakhar <prakharb5645@gmail.com>
2025-05-27 22:28:30 +0530

    yaen

---
+++ end_pyqs_2024/1_main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+    Write a program in C to remove all whitespaces in a String and print it on
+    the screen. For example given the following string as
+    input: Keep Learning Dear Students" and the program should
+    generate: "KeepLearningDearStudents" as output
+*/
+
+void removeWhitespaces(char *str)
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i]!=' ')
+        {
+            str[count++]=str[i];
+        }
+    }
+    str[count]='\0';
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter the string :");
+    gets(str);
+
+    removeWhitespaces(str);
+
+    printf("Output String :%s", str);
+
+    return 0;
+}
