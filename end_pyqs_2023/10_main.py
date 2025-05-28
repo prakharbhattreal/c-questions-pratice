195c5ca70194ea6bab65a9d87963648ac0008c38
Prakhar <prakharb5645@gmail.com>
2025-05-28 12:13:37 +0530

    ver

--- end_pyqs_2023/10_main.py
+++ end_pyqs_2023/10_main.py
@@ -1,9 +1,7 @@
 # File: sample.txt
 # Objective: Count number of lines in the file
 
-# 1. way (chatgpt)
-# Open file in read mode
-file = open("C:\\Users\\😂😂\\sample.txt")
+file = open("C:\\Users\\😂😂\\sample.txt", "r")
 data = file.read()
 lines = data.split('\n')
 print("Number of lines present in a file :", len(lines))
